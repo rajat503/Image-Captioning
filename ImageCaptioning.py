cd9a21a56b1fe33bdc1dce926ad69d8c463447de
Rajat Agarwal <rajat503@gmail.com>
2018-06-14 12:13:26 +0530

    Added attention model

--- ImageCaptioning.py
+++ ImageCaptioning.py
@@ -7,9 +7,8 @@ import numpy as np
 import tensorflow as tf
 from matplotlib import pyplot as plt
 
-NUM_EPOCHS = 2000
 BATCH_SIZE = 32
-SEQUENCE_LENGTH = 12
+SEQUENCE_LENGTH = 10
 LSTM_SIZE = 128
 
 image_to_id = {}
@@ -107,25 +106,23 @@ def process_images(image_name):
     img = cv2.resize(img, (224,224))
     return img
 
-dataset = []
-for image_name in image_to_id.keys():
-    img_id = image_to_id[image_name]
-    caption = id_to_caption[img_id]
-    dataset.append((process_images(image_name),encode_caption(caption,"input"), encode_caption(caption,"output"), caption))
-
 input_img = tf.placeholder(tf.float32, [BATCH_SIZE, 224, 224, 3])
 input_seq = tf.placeholder(tf.int32, [SEQUENCE_LENGTH, BATCH_SIZE])
 target_seq = tf.placeholder(tf.int32, [SEQUENCE_LENGTH, BATCH_SIZE])
 lr = tf.placeholder(tf.float32)
 
-preprocess_img = tf.keras.applications.resnet50.preprocess_input(input_img, mode="tf")
-resnet = tf.keras.applications.ResNet50(include_top=False, weights='imagenet',input_tensor=input_img,input_shape=(224,224,3))
-for layer in resnet.layers:
-    layer.trainable = False
+preprocess_img = tf.keras.applications.resnet50.preprocess_input(input_img)
+resnet = tf.keras.applications.ResNet50(include_top=False, weights='imagenet',input_tensor=preprocess_img,input_shape=(224,224,3))
+model_intermediate = tf.keras.Model(inputs=resnet.input, outputs=resnet.get_layer('activation_10').output)
+
+LAYER_SIZE = 55*55
+LAYER_DEPTH = 256
 img_features = tf.reshape(resnet.outputs[0], [BATCH_SIZE, 2048])
+intermediate_features = tf.reshape(model_intermediate.outputs[0], [BATCH_SIZE,LAYER_SIZE,LAYER_DEPTH])
+
 img_embedding = tf.layers.dense(img_features, GLOVE_DIM)
 
-lstm = tf.contrib.rnn.LSTMCell(LSTM_SIZE, initializer=tf.initializers.orthogonal(), activation=tf.nn.relu)
+lstm = tf.contrib.rnn.LSTMCell(LSTM_SIZE, initializer=tf.initializers.orthogonal(), activation=tf.nn.tanh)
 state = lstm.zero_state(BATCH_SIZE, tf.float32)
 lstm_output, state = lstm(img_embedding, state)
 
@@ -136,28 +133,55 @@ input_sequence = tf.unstack(word_embeddings)
 lstm_outputs = []
 
 for input_batch in input_sequence:
-    lstm_output, state = lstm(input_batch, state)
+    attention_fc = tf.layers.dense(lstm_output, 1024, activation=tf.nn.relu, name="attention_fc",reuse=tf.AUTO_REUSE)
+    attention_logit = tf.layers.dense(attention_fc, LAYER_SIZE, name="attention_logit",reuse=tf.AUTO_REUSE)
+    attention_softmax = tf.reshape(tf.nn.softmax(attention_logit), [BATCH_SIZE, 1, LAYER_SIZE])
+    attention_features = tf.reshape(tf.matmul(attention_softmax, intermediate_features), [BATCH_SIZE, LAYER_DEPTH])
+    attention_embedding = tf.layers.dense(attention_features, GLOVE_DIM, name = "attention_input_fc", reuse=tf.AUTO_REUSE)
+    word_transform = tf.layers.dense(input_batch, GLOVE_DIM, name="word_input_fc", reuse=tf.AUTO_REUSE)
+    lstm_output, state = lstm(attention_embedding+word_transform, state)
     lstm_outputs.append(lstm_output)
 
 lstm_outputs = tf.stack(lstm_outputs)
 
 fc = tf.layers.dense(lstm_outputs, 1024, activation=tf.nn.relu)
 log_probs = tf.layers.dense(fc, vocab_size)
+
+t_vars = []
+for i in tf.trainable_variables():
+    if "branch" not in i.name and "conv" not in i.name:
+        t_vars.append(i)
+
+
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=target_seq,logits=log_probs))
-update = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss)
+update = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss, var_list=t_vars)
+
 output = tf.argmax(log_probs, axis = 2)
 
+dataset = []
+for image_name in image_to_id.keys():
+    img_id = image_to_id[image_name]
+    caption = id_to_caption[img_id]
+    dataset.append((process_images(image_name),encode_caption(caption,"input"), encode_caption(caption,"output"), caption))
+
+saver = tf.train.Saver(max_to_keep=1000)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for epoch in range(NUM_EPOCHS):
+NUM_EPOCHS = 300
+RESUME_EPOCH = 90
+if RESUME_EPOCH != 0:
+    saver.restore(sess, "./v1/model"+str(RESUME_EPOCH)+".ckpt")
+dataset_train = dataset[:int(0.99*len(dataset))]
+num_examples = len(dataset_train)
+for epoch in range(RESUME_EPOCH+1, NUM_EPOCHS):
+    random.shuffle(dataset_train)
     index = 0
-    num_examples = len(dataset)
     while index + BATCH_SIZE <=  num_examples:
         x_image = []
         y_input = []
         y_target = []
-        mini_batch = dataset[index:index+BATCH_SIZE]
+        mini_batch = dataset_train[index:index+BATCH_SIZE]
         for example in mini_batch:
             x_image.append(example[0])
             if len(y_input) == 0:
@@ -172,12 +196,13 @@ for epoch in range(NUM_EPOCHS):
                                                        input_seq: y_input,
                                                        target_seq: y_target,
                                                                 lr: 0.0002})
-        if index%10000 == 0:
+        if index%20000 == 0:
             print "epoch=", epoch, "index=",index, "loss=", loss_
+    save_path = saver.save(sess, "./v1/model"+str(epoch)+".ckpt")
 
 inp =  []
 y_input = []
-example = dataset[3368]
+example = dataset[-8]
 for i in range(BATCH_SIZE):
     inp.append(example[0])
     if len(y_input) == 0:
